de62a80660c0fbc5312db59c684b3f2bc7f02365
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2017-01-29 08:02:39 +0100

    initial import

---
+++ opn_tools/opnac/OpnGrammarMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Irony;
+using Irony.Parsing;
+namespace Netfox.Opn
+{
+	public partial class OpnGrammar : Grammar 
+	{
+		public static Parser GetParser()
+		{
+			var grammar = new OpnGrammar();
+			var parser = new Parser(grammar);
+			return parser;
+		}
+	}
+}
